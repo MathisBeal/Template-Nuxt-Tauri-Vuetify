7ca4a391fba814a49715364e5a450ccc946dd457
Mathis BEAL <mathis.beal04@gmail.com>
2025-04-24 16:36:00 +0200

    Working template

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-container grid-list-md>
+      <h1 justify-center>Welcome on the Nuxt + Tauri + Vuetify template</h1>
+      <t justify-center align-center>This is a basic project with Nuxt, Tauri & Vuetify configured for quick start.</t>
+      <br />
+      <br />
+      <br />
+      <v-btn color="primary" @click="incrementCounter">Increment</v-btn>
+      <p>Counter: {{ counter }}</p>
+    </v-container>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+// import { invoke } from '@tauri-apps/api/core';
+import { invoke } from "@tauri-apps/api/core";
+import { ref } from "vue";
+
+const counter = ref(0);
+
+async function incrementCounter() {
+  console.log('Incrementing counter');
+  counter.value = await invoke('increment_counter');
+}
+
+</script>
+
+<style></style>
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,34 @@
+use std::sync::Mutex;
+use tauri::State;
+
+struct Counter {
+    value: Mutex<i32>,
+}
+
+#[tauri::command]
+fn increment_counter(counter: State<Counter>) -> i32 {
+    let mut value = counter.value.lock().unwrap();
+    *value += 1;
+    *value
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .manage(Counter {
+            value: Mutex::new(0),
+        })
+        .invoke_handler(tauri::generate_handler![increment_counter])
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
